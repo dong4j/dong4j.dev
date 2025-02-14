709747d7974b3cf845b172da8809043bcaecd42e
dong4j <dong4j@gmail.com>
2025-02-14 13:43:18 +0800

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+export const SITE_URL = "https://dong4j.dev";
+export const SITE_TITLE = "dong4j";
+export const SITE_DESCRIPTION = "dong4j's personal website and blog";
+export const OG_IMAGE = `${SITE_URL}/og.jpg`;
+
+export const NAVBAR_ITEMS = [
+    { name: "Blog", href: "/blog" },
+    { name: "Projects", href: "/projects" },
+    { name: "About", href: "/about" }
+  ];
